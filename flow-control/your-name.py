4132249a23c7e1fb07b3c94b368ddd3e7e33b76d
Kenji Hirabayashi <mkhira2@gmail.com>
2018-06-10 22:36:43 -0500

    section 2

---
+++ flow-control/your-name.py
@@ -0,0 +1,13 @@
+# name = ''
+# while name != 'your name':
+# 	print('Please type your name.')
+# 	name = input()
+# print('Thank you!')
+
+name = ''
+while True:
+	print('Please type your name.')
+	name = input()
+	if name == 'your name':
+		break
+print('Thank you!')
