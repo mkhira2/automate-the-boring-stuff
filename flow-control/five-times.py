c7f29ed9074af4866a8a201416c641afbd16acbc
Kenji Hirabayashi <mkhira2@gmail.com>
2018-06-12 21:23:12 -0500

    section 2

---
+++ flow-control/five-times.py
@@ -0,0 +1,21 @@
+# for loop with 1 argument
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+# for loop with 2 arguments
+print('My name is')
+for i in range(10, 15):
+    print('Jimmy Five Times ' + str(i))
+
+# for loop with 3 arguments
+print('My name is')
+for i in range(5, 0, -1):
+    print('Jimmy Five Times ' + str(i))
+
+#while loop
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times ' + str(i))
+    i = i + 1
