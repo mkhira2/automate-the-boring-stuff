f3a20d8bbf2071ad574671c7cf5d2150523c9f2c
Kenji Hirabayashi <mkhira2@gmail.com>
2018-06-17 20:46:52 -0500

    section 5

--- complete-program/guess-the-number.py
+++ complete-program/guess-the-number.py
@@ -1,17 +1,20 @@
 # This is a guess the number game.
 import random
+
 print('Hello. What is your name?')
 name = input().title()
+
 secretNumber = random.randint(1, 20)
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 
 # Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
-    if guessesTaken == 1: 
+for guessesTaken in range(0, 6):
+    if guessesTaken == 0: 
         print('Take a guess.')
     else:
         print('Take another guess.') 
     guess = int(input())
+
     if guess < secretNumber:
         print('Your guess is too low.')
     elif guess > secretNumber:
@@ -20,6 +23,6 @@ for guessesTaken in range(1, 7):
         break # This condition is the correct guess
 
 if guess == secretNumber:
-    print('Good job,' + name + '. You guessed my number in ' + str(guessesTaken) + ' guesses.')
+    print('Good job,' + name + '. You guessed my number in ' + str(guessesTaken + 1) + ' guesses.')
 else:
     print('Nope. The number I was thinking of was ' + str(secretNumber) + '.')
