6eb2bea7a392972090edc8196ef608b3f6805c40
Kenji Hirabayashi <mkhira2@gmail.com>
2018-07-12 23:14:30 -0500

    section 10

--- regular-expressions/regex-dot-star.py
+++ regular-expressions/regex-dot-star.py
@@ -12,4 +12,15 @@ allDigitsRegex = re.compile(r'^\d+$')
 print(allDigitsRegex.search('2349872982')) # True
 print(allDigitsRegex.search('23498xxx72982')) # False
 
-# 5.00
+atRegex = re.compile(r'.at')
+print(atRegex.findall('The cat in the hat sat on the flat mat.'))
+
+atRegex = re.compile(r'.{1,2}at')
+print(atRegex.findall('The cat in the hat sat on the flat mat.'))
+
+# dot star comin atcha
+
+name = 'First Name: Kenji Last Name: Hirabayashi'
+
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
+print(nameRegex.findall(name))
