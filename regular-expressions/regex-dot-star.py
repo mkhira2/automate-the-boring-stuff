839f9b1c9e26cb035cee47b0dcbc2ddde9edfe37
Kenji Hirabayashi <mkhira2@gmail.com>
2018-07-13 00:10:19 -0500

    section 10

--- regular-expressions/regex-dot-star.py
+++ regular-expressions/regex-dot-star.py
@@ -24,3 +24,26 @@ name = 'First Name: Kenji Last Name: Hirabayashi'
 
 nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
 print(nameRegex.findall(name))
+
+serve = '<To serve humans> for dinner.>'
+
+nongreedy = re.compile(r'<(.*?)>')
+print(nongreedy.findall(serve)) # To serve humans
+
+greedy = re.compile(r'<(.*)>')
+print(greedy.findall(serve)) # To serve humans for dinner
+
+prime = 'Serve the public trust. \nProtect the innocent. \nUphold the law.'
+print(prime)
+
+dotStar = re.compile(r'.*')
+print(dotStar.search(prime)) # Serve the public trust
+
+dogStar = re.compile(r'.*', re.DOTALL)
+print(dotStar.search(prime))
+
+vowelRegex = re.compile(r'[aeiou]') # case sensitive
+print(vowelRegex.findall('Al, why does your programming book talk about RoboCop so much?'))
+
+vowelRegex = re.compile(r'[aeiou]', re.I) # case insensitive
+print(vowelRegex.findall('Al, why does your programming book talk about RoboCop so much?'))
