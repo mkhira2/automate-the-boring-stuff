108f82079d81e88d7c445ac2e9f6b2557db8aba5
Kenji Hirabayashi <mkhira2@gmail.com>
2018-07-20 00:58:47 -0500

    section 10

--- regular-expressions/sub-method.py
+++ regular-expressions/sub-method.py
@@ -13,5 +13,14 @@ print(namesRegex.findall('Agent Alice gave the secret documents to Agent Bob.'))
 
 print(namesRegex.sub(r'Agent \1****', 'Agent Alice gave the secret documents to Agent Bob.')) # prints "Agent A**** gave...to Agent B****"
 
-# VERBOSE MODE
+# VERBOSE MODE lets you add whitespace and comments to the regex string passed to re.compile()
 
+verboseNumberRegex = re.compile(r'''
+\d\d\d # area code
+- # first dash
+\d\d\d # first 3 digits
+- # second dash
+\d\d\d\d # last 4 digits
+''', re.IGNORECASE | re.DOTALL | re.VERBOSE)
+
+print(verboseNumberRegex.findall('My office number is 512-555-1234x678'))
