77c7b064c28b87870eb1b43fad8b8bd1883152f1
Kenji Hirabayashi <mkhira2@gmail.com>
2018-07-23 23:23:28 -0500

    todo list for email and phone scraper

---
+++ regular-expressions/phone-and-email.py
@@ -0,0 +1,11 @@
+#! python3
+
+#TODO: create a regex for phone numbers
+
+#TODO: create a regex for email addresses
+
+#TODO: get the text off the clipboard
+
+#TODO: extract email/phone from this text
+
+#TODO: copy extracted email/phone to the clipboard
