bd19882180e0fd4f5ce0d61337f86370721a94c9
Kenji Hirabayashi <mkhira2@gmail.com>
2018-07-24 00:05:19 -0500

    email and phone scraper complete

--- regular-expressions/phone-and-email.py
+++ regular-expressions/phone-and-email.py
@@ -1,11 +1,46 @@
-#! python3
+import re, pyperclip
 
-#TODO: create a regex for phone numbers
+### create a regex for phone numbers ###
 
-#TODO: create a regex for email addresses
+phoneRegex = re.compile(r'''
 
-#TODO: get the text off the clipboard
+(
+((\d\d\d) | (\(\d\d\d\)))?  # area code -optional-
+(\s|-)  # first separator
+\d\d\d  # first 3 digits
+-   # separator
+\d\d\d\d  # last 4 digits
+(((ext(\.)?\s) |x)  # extension word part -optional-
+(\d{2,5}))?  # extension number part -optional
+)
 
-#TODO: extract email/phone from this text
+''', re.VERBOSE)
 
-#TODO: copy extracted email/phone to the clipboard
+### create a regex for email addresses ###
+
+emailRegex = re.compile(r'''
+
+[a-zA-Z0-9_.+]+  # name part
+@  # @ symbol
+[a-zA-Z0-9_.+]+  # domain name part
+
+''', re.VERBOSE)
+
+### get the text off the clipboard ###
+
+text = pyperclip.paste()
+
+### extract email/phone from this text ###
+
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+# copy extracted email/phone to the clipboard
+
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+
+pyperclip.copy(results)
